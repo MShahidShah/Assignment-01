a8183bde3127a013706d42164095993b41d06267
m.shahid shah <m.92shahidshah@gmail.com>
2023-09-06 01:51:12 +0500

    underconstrution this js project

---
+++ app.js
@@ -0,0 +1,28 @@
+
+// 1. Alert these following (individually):
+// I. First Name
+// II. Last Name
+// III. Email
+// IV. Phone Number
+// V. Password
+
+// ----Answer-----------
+
+// alert ("First Name");
+// alert ("Last Name");
+// alert ("E-Mail");
+// alert ("Phone Number");
+// alert ("Password");
+
+// 2. Correct this statement: alert"You're learning JavaScript!";?
+
+// alert("You're learning JavaScript!");
+
+// 3. Code an alert statement displaying any message you like?
+
+// alert ("Salam! Pakistan");
+
+
+// --------Chapter 2 (Variables for string)---------------
+
+1. Declare any variable in the camelCase format.
