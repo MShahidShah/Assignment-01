fc53bf5dd74c44c83f48e1e9d963839aad2c2348
m.shahid shah <m.92shahidshah@gmail.com>
2023-09-06 02:22:59 +0500

    second chapter complete

--- app.js
+++ app.js
@@ -16,13 +16,43 @@
 
 // 2. Correct this statement: alert"You're learning JavaScript!";?
 
+// ----Answer-----------
 // alert("You're learning JavaScript!");
 
 // 3. Code an alert statement displaying any message you like?
 
+// ----Answer-----------
 // alert ("Salam! Pakistan");
 
 
 // --------Chapter 2 (Variables for string)---------------
 
-1. Declare any variable in the camelCase format.
+// 1. Declare any variable in the camelCase format?
+
+// ----Answer-----------
+// var salamShahid;
+
+// 2. Declare a variable of your choice without defining it. Then, in a
+// second statement, assign it a string of your choice.
+
+// ----Answer-----------
+// var notDeclare;
+// notDeclare = "Shahid";
+
+// alert(notDeclare);
+// console.log(notDeclare);
+
+// 3.Declare the variable teamName and Alert your Team name.
+
+// ----Answer-----------
+// var teamName = "Pakistan";
+
+// alert(teamName);
+
+// 4. In one statement declare a variable. In a second statement assign
+// it the sum of 2 numbers.
+
+// ----Answer-----------
+// var oneStatement;
+// oneStatement = 2;
+// console.log('Sum of' + ' ' + oneStatement);
